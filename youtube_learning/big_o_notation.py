6054b6fd0fb8c5de12c177f40f4568fdf9317eb7
abangiraekyomuhendo <abangiraprecious@gmail.com>
2024-11-15 05:18:27 +0300

    big-o

--- youtube_learning/big_o_notation.py
+++ youtube_learning/big_o_notation.py
@@ -1,2 +1,15 @@
 #What is it?
 # Big O notation is used to measure how running time or space requirements grow as input size grows/increases
+
+def get_squared_numbers(numbers):
+    squared_numbers = []
+    for n in numbers:
+        squared_numbers.append(n*n)
+    return squared_numbers
+
+numbers = [2,4,8,9]
+get_squared_numbers(numbers)
+
+result = get_squared_numbers(numbers)
+
+print("squared numbers are: ", result)
