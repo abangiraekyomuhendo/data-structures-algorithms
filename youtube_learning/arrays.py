bcf2e8e87a63bfebbac7deb2074b07983b4275e6
abangiraekyomuhendo <abangiraprecious@gmail.com>
2024-11-15 04:24:15 +0300

    arrays

---
+++ youtube_learning/arrays.py
@@ -0,0 +1,7 @@
+#Store apple's stock price for 5 days and answer:
+#1. What was the price on day 1?
+#2. What was the price on day 3?
+
+stock_prices = [298,305,320,301,292]
+stock_prices[0] #to retrieve the price for day one
+stock_prices[320] # this will retrieve price from day three
