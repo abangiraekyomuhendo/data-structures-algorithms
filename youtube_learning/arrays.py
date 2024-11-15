639ce2e6a1bc0ab99289c3a11e8d149d5163851f
abangiraekyomuhendo <abangiraprecious@gmail.com>
2024-11-15 04:25:53 +0300

    arrays

--- youtube_learning/arrays.py
+++ youtube_learning/arrays.py
@@ -5,3 +5,7 @@
 stock_prices = [298,305,320,301,292]
 stock_prices[0] #to retrieve the price for day one
 stock_prices[320] # this will retrieve price from day three
+
+#on what day was the price 301?
+#looking by value
+#run a 
