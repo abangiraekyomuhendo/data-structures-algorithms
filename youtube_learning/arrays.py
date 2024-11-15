a9675c8cfb41b98f43850289254807eb1f8eebac
abangiraekyomuhendo <abangiraprecious@gmail.com>
2024-11-15 04:30:37 +0300

    arrays

--- youtube_learning/arrays.py
+++ youtube_learning/arrays.py
@@ -2,10 +2,20 @@
 #1. What was the price on day 1?
 #2. What was the price on day 3?
 
-stock_prices = [298,305,320,301,292]
-stock_prices[0] #to retrieve the price for day one
-stock_prices[320] # this will retrieve price from day three
+#stock_prices = [298,305,320,301,292]
+#stock_prices[0] #to retrieve the price for day one
+#stock_prices[320] # this will retrieve price from day three
 
 #on what day was the price 301?
 #looking by value
-#run a 
+#run a for loop to check 
+
+def find_stock_price(stock_prices):
+    for i in range(len(stock_prices)):
+        if stock_prices[i]==301:
+            return i
+    return -1
+
+result =  find_stock_price(stock_prices)
+print("The index of 301 is: ", result)   
+    
