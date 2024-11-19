769431b9e61585444ab021e2173f7d111cbb965c
abangiraekyomuhendo <abangiraprecious@gmail.com>
2024-11-20 01:58:55 +0300

    adding comments

--- lectures/data-structures/linked-lists.py
+++ lectures/data-structures/linked-lists.py
@@ -9,7 +9,7 @@ class Node:
 def append(self, data):
     new_node = Node(data) # Creating new node
     if not self.head: # if the list is empty
-        self.head = new_node
+        self.head = new_node # make the new node the head
         return
     current = self.head
     while current.next:
