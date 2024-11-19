33fcd0aaba5e7616c0ab102b0b7fed6729aa9e8f
abangiraekyomuhendo <abangiraprecious@gmail.com>
2024-11-20 01:53:52 +0300

    linked-lists

---
+++ lectures/data-structures/linked-lists.py
@@ -0,0 +1,17 @@
+#SINGLY LINKED LISTS
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def append(self, data):
+    new_node = Node(data) 
+    if not self.head:
+        self.head = new_node
+        return
+    current = self.head
+    while current.next:
+        current = current.next
+    current.next = new_node 
