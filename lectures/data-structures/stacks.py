e3f180a954c5759685d4b19bbc20cd4bf2c4a781
abangiraekyomuhendo <abangiraprecious@gmail.com>
2024-11-21 23:04:52 +0300

    more stacks

--- lectures/data-structures/stacks.py
+++ lectures/data-structures/stacks.py
@@ -1,2 +1,4 @@
 #LIFO data structure where you keep on pushing the elements and when you pop, it pops the last element that you pushed
 #Push/pop is an O(1) - constant time
+# to search an element in a stack by value, it will be O(n) - linear time
+
