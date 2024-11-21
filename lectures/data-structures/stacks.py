36db48a1e267365d675e0b490b1e319f21de8968
abangiraekyomuhendo <abangiraprecious@gmail.com>
2024-11-21 23:08:09 +0300

    more stacks

--- lectures/data-structures/stacks.py
+++ lectures/data-structures/stacks.py
@@ -2,3 +2,5 @@
 #Push/pop is an O(1) - constant time
 # to search an element in a stack by value, it will be O(n) - linear time
 
+#Use cases for stack
+# - Functin calling in any programming language is managed by stack. - Undo (Ctrl+Z) functionality in any editor uses stack to track down last set of operations
